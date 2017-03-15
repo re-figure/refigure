1d1dddcceb16b506e6c57a94bf43d278a51d8b06
Alexander Tupavov <tauinbox@gmail.com>
2017-03-15 19:12:57 +0300

    + pmc and figshare parsers

---
+++ ext/pmc.js
@@ -0,0 +1,32 @@
+function parseFigures() {
+    var figures = [];
+    //noinspection UnnecessaryLocalVariableJS
+    var dfd = new Promise(function (resolve) {
+        var authors = document.querySelector("meta[name='citation_authors']");
+        var doi = document.querySelector("meta[name='citation_doi']");
+        var src, prnt, caption, legend;
+
+        if (authors && authors.content) {authors = authors.content.replace(/,/g, ";");}
+        if (doi && doi.content) {doi = doi.content;}
+
+        for (var i = 0; i < document.images.length; i++) {
+            src = document.images[i].getAttribute("src-large");
+            if (src && src.match(/articles\/.*\/bin/)) {
+                var figure = {URL: window.location.origin + src};
+
+                prnt = document.images[i].parentNode.parentNode;
+                caption = prnt.childNodes[1].childNodes[0];
+                legend = prnt.getElementsByTagName('span')[0];
+
+                if (caption) {figure.Caption = caption.innerText;}
+                if (legend) {figure.Legend = legend.innerText;}
+                if (authors) {figure.Authors = authors;}
+                if (doi) {figure.DOI = doi;}
+                figures.push(figure);
+            }
+        }
+        resolve(figures);
+    });
+
+    return dfd;
+}

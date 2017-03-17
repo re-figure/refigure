fb065dfa11ecf590f75b6f227eb98202b88b9fbb
Alexander Tupavov <tauinbox@gmail.com>
2017-03-17 15:28:45 +0300

    + two new message types for logging events

--- ext/js/const.js
+++ ext/js/const.js
@@ -9,6 +9,9 @@ var _gConst = {
     MSG_TYPE_ADD_START: 2,
     MSG_TYPE_ADD_COMPLETED: 3,
 
+    MSG_TYPE_USER_LOGGED_IN: 4,
+    MSG_TYPE_USER_LOGGED_OUT: 5,
+
     POPUP_ERROR_FIG_NOT_PARSED: 'Figure was not parsed',
     POPUP_ERROR_FIG_DUPLICATE: 'Figure is already selected'
 };

8d653982415b4d443a861840339865601c55b44d
artemb <fac266e81a499437c2ea06f3ab958d6bb5d12c8d>
2017-03-21 16:56:57 +0300

    + --env=local switch for https://localhost:8181/api/

--- tasks/ext_manifest.js
+++ tasks/ext_manifest.js
@@ -3,9 +3,14 @@
 const gulp = require('gulp');
 const extOpt = require('js.shared').config.get('extension');
 const replace = require('gulp-batch-replace');
+const args = require('yargs').argv;
 
 module.exports = function () {
     let phs = [];
+
+    if(args.env === 'dev'){
+        extOpt.replace.REMOTE_API_URL = 'https://localhost:8181/api/'
+    }
 
     Object.keys(extOpt.replace).forEach(function (ph) {
         phs.push([ph, extOpt.replace[ph]]);

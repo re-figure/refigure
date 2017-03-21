9fade1ecb8f6f0f21c00f9e4a4856caf819526f9
Alexander Tupavov <tauinbox@gmail.com>
2017-03-21 17:26:05 +0300

    + Login form sends request to the server

--- ext/popup/app.js
+++ ext/popup/app.js
@@ -11,11 +11,12 @@ chrome.tabs.query({
 angular.module('ReFigure', [])
     .config([function () {
     }])
-    .controller('MainCtrl', ['$scope', function ($scope) {
+    .controller('MainCtrl', ['$scope', 'Authn', function ($scope, Authn) {
 
         $scope.selected = [];
         $scope.error = '';
         $scope.figCount = 0;
+        $scope.loginData = {email: 'blabla@test.org', password: 'Pa$$word'};
         $scope.isAuthenticated = false;
 
         $scope.figureAddStart = function () {
@@ -32,7 +33,17 @@ angular.module('ReFigure', [])
             });
         };
 
-        $scope.login = function () {
+        $scope.login = function (loginData) {
+            Authn.login(loginData).then(
+                function(response) {
+                    //success
+                    console.log(response.data.data);
+                },
+                function (error) {
+                    //error
+                    console.log(error);
+                }
+            );
             chrome.storage.local.set({
                 userInfo: {isAuthenticated: true}
             });
@@ -139,4 +150,11 @@ angular.module('ReFigure', [])
             return deduped;
         }
 
+    }])
+    .service('Authn', ['$http', function ($http) {
+        var authenticate = this;
+
+        authenticate.login = function (loginData) {
+            return $http.post(_gApiURL + "login", { Email: loginData.email, Password: loginData.password });
+        };
     }]);

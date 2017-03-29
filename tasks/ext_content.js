212bf40fe1422579442b7b8862a1535cfd118ca6
artemb <fac266e81a499437c2ea06f3ab958d6bb5d12c8d>
2017-03-29 19:10:31 +0300

    + tasks for content's templates, styles for dialog

--- tasks/ext_content.js
+++ tasks/ext_content.js
@@ -4,12 +4,33 @@ const gulp = require('gulp');
 const concat = require('gulp-concat');
 const sass = require('gulp-sass');
 const merge = require('merge-stream');
+const rename = require('gulp-rename');
+const htmlmin = require('gulp-htmlmin');
+const ngHtml2Js = require('gulp-ng-html2js');
+const uglify = require('gulp-uglify');
 
 const extOpt = require('./../gulp.conf').extension;
 
 module.exports = function () {
 
+    let templates = gulp
+        .src(extOpt.content.html)
+        .pipe(rename({
+            dirname: ''
+        }))
+        .pipe(htmlmin({
+            collapseWhitespace: true
+        }))
+        .pipe(ngHtml2Js({
+            moduleName: 'ReFigureContent',
+            prefix: 'view/'
+        }))
+        .pipe(concat('templates.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(extOpt.dist + '/content'));
+
     return merge(
+        templates,
         gulp
             .src(extOpt.content.mainScripts)
             .pipe(concat('content.js'))

3c23bff75da0c57940b387df36c17b5d51c1bfae
artemb <fac266e81a499437c2ea06f3ab958d6bb5d12c8d>
2017-04-12 13:14:26 +0300

    4. Copy/Paste of image attribute while adding images

--- ext/src/content/app.js
+++ ext/src/content/app.js
@@ -170,6 +170,12 @@ angular.module('ReFigureContent', [])
                     }
                     ngModel.$setViewValue(html);
                 }
+
+                element.on('paste', function(e) {
+                    e.preventDefault();
+                    var text = e.clipboardData.getData('text/plain');
+                    document.execCommand('insertHTML', false, text);
+                });
             }
         };
     }]);

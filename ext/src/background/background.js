86889f224e0a80c5aadb0ff9366ef4d8dcec7e86
artemb <fac266e81a499437c2ea06f3ab958d6bb5d12c8d>
2017-04-25 19:33:40 +0300

    + turn off extension on refigure site

--- ext/src/background/background.js
+++ ext/src/background/background.js
@@ -44,15 +44,17 @@ function startSearchFiguresIfNeed(tab) {
     if (isTabToProceed(tab)) {
         var t = tabsData[tab.id];
         if (t) {
-            if (t.url && t.url !== tab.url) {
-                // the tab URL has changed, so start search figures
-                t.status = _gConst.STATUS_INPROCESS;
-                t.url = tab.url;
-                chrome.tabs.sendMessage(tab.id, {type: _gConst.MSG_TYPE_START_SEARCH});
-            } else if (!t.url) {
-                t.status = _gConst.STATUS_INPROCESS;
-                t.url = tab.url;
-                chrome.tabs.sendMessage(tab.id, {type: _gConst.MSG_TYPE_START_SEARCH});
+            if (!tab.url.match(/refigure\./)) {
+                if (t.url && t.url !== tab.url) {
+                    // the tab URL has changed, so start search figures
+                    t.status = _gConst.STATUS_INPROCESS;
+                    t.url = tab.url;
+                    chrome.tabs.sendMessage(tab.id, {type: _gConst.MSG_TYPE_START_SEARCH});
+                } else if (!t.url) {
+                    t.status = _gConst.STATUS_INPROCESS;
+                    t.url = tab.url;
+                    chrome.tabs.sendMessage(tab.id, {type: _gConst.MSG_TYPE_START_SEARCH});
+                }
             }
         }
     }

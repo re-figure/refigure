0cf584746aa42569c4cea1de933a47bee57bacc9
artemb <fac266e81a499437c2ea06f3ab958d6bb5d12c8d>
2017-04-28 18:55:23 +0300

    ~ oauth params moved to package.json, replace added to js task

--- front/app/components/auth/auth.module.js
+++ front/app/components/auth/auth.module.js
@@ -13,25 +13,44 @@
             'router.helper',
             'ngCookies',
             'refigureShared',
-            'google-signin'
+            'google-signin',
+            'ngFacebook'
         ])
         .constant('authApiUri', '/api')
+        .constant('OAuthCfg', {
+            google: {
+                clientId: 'GOOGLE_CLIENT_ID',
+                apiKey: 'GOOGLE_API_KEY'
+            },
+            facebook: {
+                clientId: 'FACEBOOK_CLIENT_ID',
+            }
+        })
         .config(Config)
         .run(Run);
 
-    Config.$inject = ['GoogleSigninProvider'];
+    Config.$inject = ['GoogleSigninProvider', '$facebookProvider', 'OAuthCfg'];
 
-    function Config(GoogleSigninProvider) {
+    function Config(GoogleSigninProvider, $facebookProvider, OAuthCfg) {
         GoogleSigninProvider.init({
-            apiKey: 'AIzaSyCnDKJt_n3eS3QtqLqcTkMu2vaCaguPCqU',
-            clientId: '604123564572-uuu98pul48vj6t2uqgu2epi8723egmli.apps.googleusercontent.com',
+            apiKey: OAuthCfg.google.apiKey,
+            clientId: OAuthCfg.google.clientId,
             scope: 'profile'
         });
+
+        //$facebookProvider.setAppId('<your-facebook-app-id>');
     }
 
     Run.$inject = ['$rootScope', 'GoogleSignin', 'auth'];
 
     function Run($rootScope, GoogleSignin, auth) {
+        // var js = document.createElement('script'),
+        //     fjs = document.getElementsByTagName('script')[0];
+        //
+        // js.id = 'facebook-jssdk';
+        // js.src = '//connect.facebook.net/en_US/sdk.js';
+        // fjs.parentNode.insertBefore(js, fjs);
+
         $rootScope.$on('ng-google-signin:isSignedIn', function (event, isSignedIn) {
             if (isSignedIn && !auth.isAuthenticated()) {
                 auth.oAuthGoogle(GoogleSignin.getUser().getAuthResponse()['id_token']);

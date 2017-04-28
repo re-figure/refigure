88aa7095ecf94395e88c3f94c53a22ca8304cc4b
artemb <fac266e81a499437c2ea06f3ab958d6bb5d12c8d>
2017-04-28 13:20:13 +0300

    ~ google client id to global config

--- back/oauth.js
+++ back/oauth.js
@@ -1,25 +1,26 @@
 'use strict';
 
-
 const constants = require('./const');
 const httpStatus = require('http-status-codes');
+const config = require('js.shared').config;
+const db = require('./db');
 
-//google
-const GOOGLE_CLIENT_ID = '604123564572-uuu98pul48vj6t2uqgu2epi8723egmli.apps.googleusercontent.com';
 const GoogleAuth = require('google-auth-library');
-const auth = new GoogleAuth;
-const client = new auth.OAuth2(GOOGLE_CLIENT_ID, '', '');
 
 exports.google = oAuthGoogle;
 
 function oAuthGoogle(req, res) {
+    let CLIENT_ID = config.get('oauth.google.clientId');
+    let gAuth = new GoogleAuth();
+    let gClient = new gAuth.OAuth2(CLIENT_ID, '', '');
+
     if (!req.params.token) {
         res.status(httpStatus.BAD_REQUEST).json({
             error: constants.ERROR_BADPARAMETERS,
             message: constants.ERROR_MSG_BADPARAMETERS
         });
     }
-    client.verifyIdToken(req.params.token, GOOGLE_CLIENT_ID, (e, login) => {
+    gClient.verifyIdToken(req.params.token, CLIENT_ID, (e, login) => {
         let payload = login.getPayload();
         console.log(payload);
         res.send(payload);

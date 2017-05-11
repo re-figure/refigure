28fd33083111c6eb1b558a45dfc8fde33dcfc801
artemb <fac266e81a499437c2ea06f3ab958d6bb5d12c8d>
2017-05-11 19:16:13 +0300

    ~ parser refactoring, plos: thumbnails are parsed too

--- ext/src/content/parsers/plos.js
+++ ext/src/content/parsers/plos.js
@@ -1,58 +1,43 @@
-CONTENT_BLOCK_SELECTOR = '.article-text';
+(function (parser) {
+    parser.CONTENT_BLOCK_SELECTOR = '.article-text';
 
-function parseFigures() {
-    var figures = [],
-        pageDOI = getPageDOI(),
-        Authors = getAuthors();
+    parser.parseFigures = function () {
+        var figures = [],
+            pageDOI = parser.getPageDOI(),
+            Authors = parser.getAuthors();
 
-    Sizzle(CONTENT_BLOCK_SELECTOR + ' .figure[data-doi]').forEach(function (figure) {
-        var DOIFigure = figure.dataset.uri ? figure.dataset.uri.replace(/^info:doi\//, '') : figure.dataset.doi,
-            figureImage = Sizzle('.img-box img', figure);
-        if (figureImage.length !== 1) {
-            logError('Figure has ', figureImage.length, 'images');
-        } else {
-            var Legend = Sizzle('>p:not([class])', figure).map(function (tag) {
-                return prepareContent(tag);
-            }).join('');
-            figures.push({
-                URL: figureImage[0].src,   //.replace('size=inline', 'size=large') collecting only large images
-                Caption: getFigureCaption(figure, figureImage[0].title),
-                Legend: Legend,
-                Authors: Authors,
-                DOI: pageDOI,
-                DOIFigure: DOIFigure
-            });
+        Sizzle(parser.CONTENT_BLOCK_SELECTOR + ' .figure[data-doi]').forEach(function (figure) {
+            var DOIFigure = figure.dataset.uri ? figure.dataset.uri.replace(/^info:doi\//, '') : figure.dataset.doi,
+                figureImage = Sizzle('.img-box img', figure);
+            if (figureImage.length !== 1) {
+                window.logError('Figure has ', figureImage.length, 'images');
+            } else {
+                var Legend = Sizzle('>p:not([class])', figure).map(function (tag) {
+                    return parser.prepareContent(tag);
+                }).join('');
+                figures.push({
+                    URL: parser.srcTransformer(figureImage[0].src),
+                    Caption: getFigureCaption(figure, figureImage[0].title),
+                    Legend: Legend,
+                    Authors: Authors,
+                    DOI: pageDOI,
+                    DOIFigure: DOIFigure
+                });
+            }
+        });
+
+        return Promise.resolve(figures);
+
+        /////////////////////////////
+
+        function getFigureCaption(container, title) {
+            var selector = Sizzle('.figcaption', container);
+            return selector.length ? parser.prepareContent(selector[0]) : title;
         }
-    });
+    };
 
-    return Promise.resolve(figures);
+    parser.srcTransformer = function (src) {
+        return src.replace('info:doi/', '');
+    };
 
-    /////////////////////////////
-
-    function getFigureCaption(container, title) {
-        var selector = Sizzle('.figcaption', container);
-        return selector.length ? prepareContent(selector[0]) : title;
-    }
-
-    function getAuthors() {
-        var authorMeta = Sizzle('meta[name="citation_author"]');
-        if(!authorMeta.length){
-            authorMeta = Sizzle('meta[name="DC.contributor"]');
-        }
-        return authorMeta.map(function (el) {
-            return el.content;
-        }).join('; ');
-    }
-
-    function getPageDOI() {
-        var metaDOI = Sizzle('meta[name="citation_doi"]'),
-            ret = '';
-        if (metaDOI.length !== 1) {
-            logError('citation_doi meta length is ', metaDOI.length);
-        } else {
-            ret = metaDOI[0].content;
-        }
-        return ret;
-    }
-
-}
+})(window.parser);
--- ext/src/content/parsers/pmc.js
+++ ext/src/content/parsers/pmc.js
@@ -1,35 +1,39 @@
-CONTENT_BLOCK_SELECTOR = '.jig-ncbiinpagenav';
+(function (parser) {
+    parser.CONTENT_BLOCK_SELECTOR = '.jig-ncbiinpagenav';
 
-function parseFigures() {
-    var figures = [];
-    //noinspection UnnecessaryLocalVariableJS
-    var authors = document.querySelector("meta[name='citation_authors']");
-    var doi = document.querySelector("meta[name='citation_doi']");
-    var src, figureBlock, caption, legend;
+    parser.parseFigures = function () {
+        var figures = [];
+        //noinspection UnnecessaryLocalVariableJS
+        var Authors = parser.getAuthors();
+        var pageDOI = parser.getPageDOI();
+        var src, figureBlock, caption, legend;
 
-    if (authors && authors.content) {authors = authors.content.replace(/,/g, ";");}
-    if (doi && doi.content) {doi = doi.content;}
+        for (var i = 0; i < document.images.length; i++) {
+            src = document.images[i].src;
+            //src = document.images[i].getAttribute("src-large");
+            if (src && src.match(/articles\/.*\/bin/) && document.images[i].hasAttribute('src-large')) {
+                var figure = {URL: src};
 
-    for (var i = 0; i < document.images.length; i++) {
-        src = document.images[i].src;
-        //src = document.images[i].getAttribute("src-large");
-        if (src && src.match(/articles\/.*\/bin/) && document.images[i].hasAttribute("src-large")) {
-            var figure = {URL: src};
+                figureBlock = document.images[i].parentNode.parentNode;
 
-            figureBlock = document.images[i].parentNode.parentNode;
+                caption = figureBlock.querySelector('div.icnblk_cntnt > div:nth-child(1) > a');
+                caption = caption ? caption.innerText : null;
+                legend = figureBlock.querySelector('div.icnblk_cntnt > div:nth-child(2) > span');
+                legend = legend ? legend.innerText : null;
 
-            caption = figureBlock.querySelector("div.icnblk_cntnt > div:nth-child(1) > a");
-            caption = caption ? caption.innerText : null;
-            legend = figureBlock.querySelector("div.icnblk_cntnt > div:nth-child(2) > span");
-            legend = legend ? legend.innerText : null;
-
-            if (caption) {figure.Caption = caption;}
-            if (legend) {figure.Legend = legend;}
-            if (authors) {figure.Authors = authors;}
-            if (doi) {figure.DOI = doi;}
-            figures.push(figure);
+                if (caption) {
+                    figure.Caption = caption;
+                }
+                if (legend) {
+                    figure.Legend = legend;
+                }
+                figure.Authors = Authors;
+                figure.DOI = pageDOI;
+                figures.push(figure);
+            }
         }
-    }
 
-    return Promise.resolve(figures);
-}
+        return Promise.resolve(figures);
+    };
+
+})(window.parser);

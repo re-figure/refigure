01ccacb991bbe8797ca0d70ccfc5d03f64d3ae20
Andrey Pavlov <andrew.m.pavlov@gmail.com>
2017-08-30 17:29:29 -0400

    code deploy minor fix

--- aws/codedeploy/app-start.sh
+++ aws/codedeploy/app-start.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+#register service
+src="/var/www/refigure.org"
+cp -f $src/aws/etc/init/refigure.conf /etc/init/
+
+#start service
 initctl start refigure
--- aws/codedeploy/app-stop.sh
+++ aws/codedeploy/app-stop.sh
@@ -6,7 +6,3 @@ if (( $(ps -ef | grep -v grep | grep $service | wc -l) > 0 )); then
  initctl stop $service
  echo $service" stopped"
 fi
-
-#register service
-src="/var/www/refigure.org"
-cp -f $src/aws/etc/init/refigure.conf /etc/init/

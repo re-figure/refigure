c4be4e22fd14524d7e5a8006694e7f8201d50524
artemb <fac266e81a499437c2ea06f3ab958d6bb5d12c8d>
2018-03-21 15:34:51 +0300

    #41 .matches polyfill for IE added

--- front/app/shared/components/collection-row/uiSrefDisabled.directive.js
+++ front/app/shared/components/collection-row/uiSrefDisabled.directive.js
@@ -30,4 +30,20 @@
         };
     }
 
+    // .matches polyfill for IE (even IE11)
+    if (!Element.prototype.matches) {
+        Element.prototype.matches =
+            Element.prototype.matchesSelector ||
+            Element.prototype.mozMatchesSelector ||
+            Element.prototype.msMatchesSelector ||
+            Element.prototype.oMatchesSelector ||
+            Element.prototype.webkitMatchesSelector ||
+            function(s) {
+                var matches = (this.document || this.ownerDocument).querySelectorAll(s),
+                    i = matches.length;
+                while (--i >= 0 && matches.item(i) !== this) {}
+                return i > -1;
+            };
+    }
+
 })(window.angular);
